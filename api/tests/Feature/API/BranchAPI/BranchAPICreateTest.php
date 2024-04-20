e8cae31d084a73146e6bbb385640f06b125d9cad
GitzJoey <gitzjoey@live.com>
2024-04-20 13:39:53 +0700

    initial commit

--- api/tests/Feature/API/BranchAPI/BranchAPICreateTest.php
+++ api/tests/Feature/API/BranchAPI/BranchAPICreateTest.php
@@ -82,8 +82,6 @@ class BranchAPICreateTest extends APITestCase
 
         $api = $this->json('POST', route('api.post.db.company.branch.save'), $branchArr);
 
-        dd($api);
-
         $api->assertSuccessful();
         $this->assertDatabaseHas('branches', [
             'company_id' => $company->id,
