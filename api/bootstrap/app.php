4dcee3f8dd7cd43522cd60519c72602203a27ac0
GitzJoey <gitzjoey@live.com>
2024-04-30 12:18:36 +0700

    initial commit

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->append([
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
