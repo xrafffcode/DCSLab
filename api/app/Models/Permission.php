dbc72ca9acf1bc03d45341a14f679e9d59d18434
GitzJoey <gitzjoey@live.com>
2024-05-01 11:39:49 +0700

    initial commit

--- api/app/Models/Permission.php
+++ api/app/Models/Permission.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laratrust\Models\Permission as PermissionModel;
 
 class Permission extends PermissionModel
 {
+    use HasFactory;
+
     public $guarded = [];
 }
