4c96a264fe1d55eccdc4ec169150320c41c1beb4
GitzJoey <gitzjoey@live.com>
2024-05-04 10:02:02 +0700

    initial commit

--- web/src/components/Base/Pagination/Link.vue
+++ web/src/components/Base/Pagination/Link.vue
@@ -5,6 +5,7 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { toRef } from "vue";
 import _ from "lodash";
 import { twMerge } from "tailwind-merge";
 import { computed, type LiHTMLAttributes, useAttrs } from "vue";
@@ -15,17 +16,19 @@ interface LinkProps extends /* @vue-ignore */ LiHTMLAttributes {
   active?: boolean;
 }
 
-const { as, active } = withDefaults(defineProps<LinkProps>(), {
+const props = withDefaults(defineProps<LinkProps>(), {
   as: "a",
   active: false,
 });
 
+const active = toRef(props, 'active');
+
 const attrs = useAttrs();
 
-const computedClass = computed(() =>
+const computedClass = computed(() => 
   twMerge([
     "min-w-0 sm:min-w-[40px] shadow-none font-normal flex items-center justify-center border-transparent text-slate-800 sm:mr-2 dark:text-slate-300 px-1 sm:px-3",
-    active && "!box font-medium dark:bg-darkmode-400",
+    active.value && "!box font-medium dark:bg-darkmode-400",
     typeof attrs.class === "string" && attrs.class,
   ])
 );
@@ -33,7 +36,7 @@ const computedClass = computed(() =>
 
 <template>
   <li class="flex-1 sm:flex-initial">
-    <Button :as="as" :class="computedClass" v-bind="_.omit(attrs, 'class')">
+    <Button :as="props.as" :class="computedClass" v-bind="_.omit(attrs, 'class')">
       <slot></slot>
     </Button>
   </li>
